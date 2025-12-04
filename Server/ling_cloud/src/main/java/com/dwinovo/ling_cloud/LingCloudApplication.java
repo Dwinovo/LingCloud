4012e89977e011dbd6dd90a889497629cf5107c5
Dwin <dwin@bupt.edu.cn>
2025-12-04 16:52:30 +0800

    init server

---
+++ Server/ling_cloud/src/main/java/com/dwinovo/ling_cloud/LingCloudApplication.java
@@ -0,0 +1,13 @@
+package com.dwinovo.ling_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LingCloudApplication.class, args);
+	}
+
+}
